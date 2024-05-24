8dc68fe4b2ce1699f2686e0bfb8656402f676602
sapiens19 <sapiens19@pop-os.localdomain>
2024-05-24 17:44:23 -0300

    Added diferents properties into Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+/* eslint-disable react/jsx-key */
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
 
 import './global.css'
 import styles from './App.module.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/125615615?v=4",
+      name: "Eduardo Kmiecik",
+      role: "Web developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-05-24 16:00:00")
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/aleeixoj.png",
+      name: "Aleixo Junior",
+      role: "Web developer senior"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-05-20 16:00:00")
+  }
+]
 
 export function App() {
 
@@ -14,8 +47,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
