4bbe87be0eb1fde02e0242433008a8afa44ee627
sapiens19 <sapiens19@pop-os.localdomain>
2024-05-26 19:29:50 -0300

    Added posibilitie of delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,11 @@ import { ThumbsUp, Trash } from "phosphor-react"
 import styles from "./Comment.module.css"
 import { Avatar } from "./Avatar"
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/125615615?v=4" />
@@ -17,7 +21,7 @@ export function Comment({ content }) {
               <time title="20 de Maio às 15h" dateTime="2024-05-20 15:18:50">Publicado há 1h</time>
             </div>
 
-            <button title="Deletar Comentário">
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
               <Trash size={24} />
             </button>
           </header>
